a61e7df9bb75987bb1526311c0ae72a0c987742c
Gkemhcs <gudikotieswarmani@gmail.com>
2022-08-27 17:56:58 +0530

    friends

--- file.js
+++ file.js
@@ -1,7 +1,7 @@
 const express=require("express")
 const app=express()
 app.get("/",(req,res)=>{
-res.send("<h1>HELLO WORLD</h1>")
+res.send("<h1>HELLO FRIENDS</h1>")
 })
 
 app.listen(process.env.PORT||8080,()=>{
