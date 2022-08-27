0375d5a83885b4ee5a8353d9ef6e22bd49be34d0
Gkemhcs <gudikotieswarmani@gmail.com>
2022-08-27 17:28:59 +0530

    lo

--- file.js
+++ file.js
@@ -4,6 +4,6 @@ app.get("/",(req,res)=>{
 res.send("<h1>HELLO WORLD</h1>")
 })
 
-app.listen(process.env.PORT,()=>{
+app.listen(process.env.PORT||8080,()=>{
 console.log("server running on port 8080")
 })
