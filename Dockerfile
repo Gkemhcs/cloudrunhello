2c23d3a3d6691827049dab2fef183109daf1afe1
Gkemhcs <gudikotieswarmani@gmail.com>
2022-08-27 14:24:04 +0530

    hello

---
+++ Dockerfile
@@ -0,0 +1,7 @@
+FROM ubuntu
+RUN mkdir app
+WORKDIR ./app
+COPY package* .
+RUN npm ci
+COPY * *
+CMD ["node","file.js"]
---
+++ file.js
@@ -0,0 +1,9 @@
+const express=require("express")
+const app=express()
+app.get("/",(req,res)=>{
+res.send("<h1>HELLO WORLD</h1>")
+})
+
+app.listen(process.env.PORT,()=>{
+console.log("server running on port 8080")
+})
