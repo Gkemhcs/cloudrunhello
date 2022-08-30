589b342c9cc0adab2538d907dc42878a24802790
Gkemhcs <97238758+Gkemhcs@users.noreply.github.com>
2022-08-30 06:32:58 +0530

    Update file.js

--- file.js
+++ file.js
@@ -2,7 +2,9 @@ const express=require("express")
 const app=express()
 app.get("/",(req,res)=>{
   console.log(req)
+  console.log(process.env.type)
 res.sendFile(__dirname+"/index.html")
+ 
 })
 
 app.listen(process.env.PORT||8080,()=>{
